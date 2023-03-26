34edf48546f9336942c47da69ce1d00686b7cd71
Wan1027 <rladhkstmd10@daum.net>
2023-03-26 23:47:00 +0900

    beforepull

---
+++ src/main/java/study/board/security/SecretKey.java
@@ -0,0 +1,12 @@
+package study.board.security;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+
+@Getter
+public class SecretKey {
+
+    private final String key = "showMeTheMoney";
+
+
+}
